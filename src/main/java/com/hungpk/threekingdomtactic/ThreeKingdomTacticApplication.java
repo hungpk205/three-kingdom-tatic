3cb4997236751b0cc8592b3a7b442237728f8a70
hungpk205 <phankieuhunght205@gmail.com>
2021-04-30 23:55:10 +0700

    Init project

---
+++ src/main/java/com/hungpk/threekingdomtactic/ThreeKingdomTacticApplication.java
@@ -0,0 +1,13 @@
+package com.hungpk.threekingdomtactic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeKingdomTacticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreeKingdomTacticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hungpk/threekingdomtactic/ThreeKingdomTacticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungpk.threekingdomtactic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeKingdomTacticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
